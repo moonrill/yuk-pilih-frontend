6365e99d4ae724c994fc9e487176c16d67ecd36c
moonrill <arilramadani245@gmail.com>
2023-11-11 05:32:45 +0700

    feat : move config

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,17 +1,17 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
-import { appConfig } from '../config/appConfig';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { appConfig } from "../../config/appConfig";
 
 export const authApi = createApi({
-  reducerPath: 'authApi',
+  reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
     baseUrl: appConfig.apiUrl,
-    jsonContentType: 'application/json',
+    jsonContentType: "application/json",
   }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: ({ username, password }) => ({
-        url: '/auth/login',
-        method: 'POST',
+        url: "/auth/login",
+        method: "POST",
         body: { username, password },
       }),
     }),
--- src/services/pollApi.js
+++ src/services/pollApi.js
@@ -1,21 +1,24 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
-import { appConfig } from '../config/appConfig';
-import token from '../utils/token';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
+import { appConfig } from "../../config/appConfig";
+import token from "../utils/token";
 
 export const pollApi = createApi({
-  reducerPath: 'pollApi',
-  baseQuery: fetchBaseQuery({baseUrl: appConfig.apiUrl, jsonContentType: 'application/json'}),
+  reducerPath: "pollApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: appConfig.apiUrl,
+    jsonContentType: "application/json",
+  }),
   endpoints: (builder) => ({
     getPolls: builder.query({
       query: () => ({
-        url: '/poll',
-        method: 'GET',
+        url: "/poll",
+        method: "GET",
         headers: {
-          'Authorization': `Bearer ${token.get('access_token')}`
+          Authorization: `Bearer ${token.get("access_token")}`,
         },
-      })
-    })
-  })
-})
+      }),
+    }),
+  }),
+});
 
-export const {useGetPollsQuery} = pollApi;
+export const { useGetPollsQuery } = pollApi;
